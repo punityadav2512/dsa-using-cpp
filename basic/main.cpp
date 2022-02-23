b3bc647746635f58cfe53318f7128553539f6b92
punityadav2512 <b520040@iiit-bh.ac.in>
2022-02-23 16:04:20 +0530

    day 0

---
+++ basic/main.cpp
@@ -0,0 +1,28 @@
+// What is iostream
+// What is namespace std
+// Difference between cin, cout, cerr, clog
+// Meaning of <<
+// Which is better \n or endl and their difference
+// Why to use single quotation while declaring char
+// cin.get()
+// ASCII Table
+// Binary and Decimal
+// Difference between char array and string
+// cin.getline()
+// custom delimeter
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "你好" << endl;
+    while (true)
+    {
+        int *p = new int;
+    }
+    // char c = 'c';
+    // cout << c << endl;
+
+    return 0;
+}
